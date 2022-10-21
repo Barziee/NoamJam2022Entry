0ac06d2e6e27aa1d120324ebd43135ebe7976e7b
BurnEmDown <amitreuveni@gmail.com>
2022-10-21 13:47:57 +0300

    Added start of insectinvader minigame

---
+++ Assets/Scripts/InsectInvaders.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InsectInvaders : Minigame
+{
+        private List<Insect> insectList;
+
+        [SerializeField] private GameObject playerObject;
+
+        public override void Init(int seconds)
+        {
+                base.Init(seconds);
+                playerObject.SetActive(true);
+        }
+
+        public void EndGame()
+        {
+                playerObject.SetActive(false);
+        }
+}
---
+++ Assets/Scripts/Spritzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class Spritzer : MonoBehaviour
+{
+    [SerializeField] 
+    private float speed = 500f;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // move left
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            // move right
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            // shoot water drop
+        }
+    }
+}
