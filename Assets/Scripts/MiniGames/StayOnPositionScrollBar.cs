a175de2817c0af3d11ef2c7739bf39fdff00264e
GalStilkol <Stilkolgal@gmail.com>
2022-10-22 14:24:52 +0300

    dead branch updates, visual fixed with barzies assets

--- Assets/Scripts/MiniGames/StayOnPositionScrollBar.cs
+++ Assets/Scripts/MiniGames/StayOnPositionScrollBar.cs
@@ -5,29 +5,53 @@ using UnityEngine.UI;
 
 public class StayOnPositionScrollBar : MonoBehaviour
 {
-    [SerializeField] float targetSpeed=5;
+    [SerializeField] Transform topBounds;
+    [SerializeField] Transform bottomBounds;
+
+
     [SerializeField] Scrollbar stayOnPosScroller;
     [SerializeField] float smoothMotion;
+    [SerializeField] float timerRandomizer=3;
 
-    float timerRandomizer;
-
-    float wantedPosition;
+    [Header("Target")]
+    float targetWantedPosition;
     float stayInSingleSpotTimer;
-
-    float targetNewRandYVal;
+    float targetSpeed;
+    float targetNewRandXVal;
     private RectTransform stayOnPosScrollerRectTransform;
+    private float pullScrollerYHeight;
+
+    [Header("Player")]
+    [SerializeField] RectTransform playerRectTransform;
+    [SerializeField] float playerGravity=0.5f;
+    [SerializeField] float playerSpeed=0.1f;
+
+
+    private float playerPullVelocity;
+    float playerWantedPosition;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         stayOnPosScrollerRectTransform = stayOnPosScroller.GetComponent<RectTransform>();
+        pullScrollerYHeight = stayOnPosScrollerRectTransform.sizeDelta.y;
+
+        Init();
+    }
+
+    public void Init()
+    {
+        CanPlayerMove = true;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         MoveTarget();
-    }
+        PlayerMove();
+        
 
    void MoveTarget()
     {
@@ -37,9 +61,47 @@ public class StayOnPositionScrollBar : MonoBehaviour
             //get new pos
             //reset timer
             stayInSingleSpotTimer= Random.value* timerRandomizer;
-            targetNewRandYVal = Random.value;
+            targetNewRandXVal = Random.value;
         }
-        wantedPosition = Mathf.SmoothDamp(stayOnPosScroller.value, targetNewRandYVal, ref targetSpeed, smoothMotion);
-        stayOnPosScroller.value = Mathf.Lerp(0, 1, wantedPosition);
+            targetWantedPosition = Mathf.SmoothDamp(stayOnPosScroller.value, targetNewRandXVal, ref targetSpeed, smoothMotion);
+        stayOnPosScroller.value = Mathf.Lerp(0, 1, targetWantedPosition);
     }
+}
+
+    private void PlayerMove()
+    {
+        if (Input.GetMouseButton(0) && CanPlayerMove)
+        {
+            playerPullVelocity += playerSpeed * Time.deltaTime;
+        }
+        playerPullVelocity -= playerGravity * Time.deltaTime;
+
+        playerWantedPosition += playerPullVelocity;
+
+        playerWantedPosition = Mathf.Clamp(playerWantedPosition, playerRectTransform.sizeDelta.x / 2, stayOnPosScrollerRectTransform.sizeDelta.x- playerRectTransform.sizeDelta.x / 2);
+
+        float newXPos = Mathf.Lerp(0, 1600, playerWantedPosition);
+
+
+
+        playerRectTransform.anchoredPosition = new Vector3(
+        newXPos,
+        playerRectTransform.position.y,
+        playerRectTransform.position.z);
+
+      /*  playerWantedPosition += playerPullVelocity;
+
+        float playerTopBound = pullScrollerYHeight / 2 - playerRectTransform.sizeDelta.y / 2;
+        float plyerBottomBound =playerRectTransform.sizeDelta.y / 2;
+
+       
+        playerWantedPosition = Mathf.Clamp(playerWantedPosition, plyerBottomBound, playerTopBound);
+
+        playerRectTransform.localPosition = Vector3.Lerp(topBounds.position, bottomBounds.position, playerWantedPosition);
+*/
+    
+    }
+
+    public bool CanPlayerMove { get; set; } = false;
+
 }
--- Assets/Scripts/MiniGames/Watering/WateringMiniGame.cs
+++ Assets/Scripts/MiniGames/Watering/WateringMiniGame.cs
@@ -45,7 +45,8 @@ public class WateringMiniGame : Minigame
       float randFreq =  UnityEngine.Random.Range(1, 5);
      
       float targetRandSize = UnityEngine.Random.Range(0.05f, 0.5f);
-        float targetRandYPos = UnityEngine.Random.Range(0f, 1f);
+        float halfSize = targetRandSize/2;
+        float targetRandYPos = UnityEngine.Random.Range(halfSize, 1- halfSize);
         hitPositionScrollBar.CreateNewBar(freq: randFreq,targetSize: targetRandSize,targetYPos: targetRandYPos);
     }
 
